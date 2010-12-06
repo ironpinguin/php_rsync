42677d7b4ee86fec378c3a90b6ad7e63f6def8b4
ironpinguin <michele@catalano.de>
2010-12-06 13:49:04 +0100

    Extension Description for use in eclipse/ZendStudio.

---
+++ rsync_extension.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This is a placeholder for the php_rsync extension where is a wrapper extension for the librsync library
+ */
+
+// Completed successfully.
+define("RSYNC_DONE", 0);
+// Blocked waiting for more data.
+define("RSYNC_BLOCKED", 1);
+// Not yet finished or blocked.
+// This value should never be returned
+// to the caller.
+define("RSYNC_RUNNING", 2);
+// Test neither passed or failed.
+define("RSYNC_TEST_SKIPPED", 77);
+// Error in file or network IO.
+define("RSYNC_IO_ERROR", 100);
+// Command line syntax error.
+define("RSYNC_SYNTAX_ERROR", 101);
+// Out of memory.
+define("RSYNC_MEM_ERROR", 102);
+// End of input file, possibly
+// unexpected.
+define("RSYNC_INPUT_ENDED", 103);
+// Bad magic number at start of
+// stream.  Probably not a librsync
+// file, or possibly the wrong kind of
+// file or from an incompatible
+// library version.
+define("RSYNC_BAD_MAGIC", 104);
+// Author is lazy.
+define("RSYNC_UNIMPLEMENTED", 105);
+// Unbelievable value in stream.
+define("RSYNC_CORRUPT", 106);
+// Probably a library bug.
+define("RSYNC_INTERNAL_ERROR", 107);
+// Bad value passed in to library,
+// probably an application bug.
+define("RSYNC_PARAM_ERROR", 108);
+
+// System is unusable
+define("RSYNC_LOG_EMERG", 0);
+// Action must be taken immediately 
+define("RSYNC_LOG_ALERT", 1);
+// Critical conditions
+define("RSYNC_LOG_CRIT", 2);
+// Error conditions
+define("RSYNC_LOG_ERR", 3);
+// Warning conditions
+define("RSYNC_LOG_WARNING", 4);
+// Normal but significant condition
+define("RSYNC_LOG_NOTICE", 5);
+// Informational
+define("RSYNC_LOG_INFO", 6);
+// Debug-level messages
+define("RSYNC_LOG_DEBUG", 7);
+
+define("RSYNC_MD4_LENGTH", 16);
+define("RSYNC_DEFAULT_STRONG_LEN", 8);
+define("RSYNC_DEFAULT_BLOCK_LEN", 2048);
+
+/**
+ * Funtion to generate the signaturfile from a /**
+ *
+ * @param php_stream|string $file         Here give the filepath or a stream for the file to read.
+ * @param php_stream|string $signaturfile Here give the filepath or a stream for the file to store.
+ */
+function rsync_generate_signature($file, $signaturfile) {}
+
+/**
+ * Function to patch a file with the given delta patch
+ * 
+ * @param php_stream|string $file
+ * @param php_stream|string $patch
+ * @param php_stream|string $newfile
+ */
+function rsync_patch_file($file, $patch, $newfile) {}
+
+/**
+ * Function to generate a patch file from the given signaturfile and the local file.
+ * 
+ * @param php_stream|string $signaturfile
+ * @param php_stream|string $file
+ * @param php_stream|string $patchfile
+ */
+function rsync_generate_delta($signaturfile, $file, $patchfile) {}
